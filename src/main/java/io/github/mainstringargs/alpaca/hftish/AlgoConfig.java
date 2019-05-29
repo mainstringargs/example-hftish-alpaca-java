aba01bc887b27ec1b4e0ff947a4d79079abbc7ad
mainstringargs <mainstringargs@users.noreply.github.com>
2019-05-29 13:11:38 -0500

    Adding command line parser

---
+++ src/main/java/io/github/mainstringargs/alpaca/hftish/AlgoConfig.java
@@ -0,0 +1,51 @@
+package io.github.mainstringargs.alpaca.hftish;
+
+import picocli.CommandLine.Option;
+
+/**
+ * The Class AlgoConfig.
+ */
+class AlgoConfig {
+
+
+
+  /** The symbol. */
+  @Option(names = {"-s", "--symbol"}, defaultValue = "SNAP",
+      description = "the stock to trade (defaults to \"SNAP\"")
+  String symbol;
+
+  /** The quantity. */
+  @Option(names = {"-q", "--quantity"}, defaultValue = "500",
+      description = "the maximum number of shares to hold at once. Note that this does not account for any existing position; the algorithm only tracks what is bought as part of its execution. (Default 500, minimum 100.)")
+  int quantity;
+
+  /**
+   * Gets the symbol.
+   *
+   * @return the symbol
+   */
+  public String getSymbol() {
+    return symbol;
+  }
+
+  /**
+   * Gets the quantity.
+   *
+   * @return the quantity
+   */
+  public int getQuantity() {
+    return quantity;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    return "AlgoConfig [symbol=" + symbol + ", quantity=" + quantity + "]";
+  }
+
+
+}
--- src/main/java/io/github/mainstringargs/alpaca/hftish/Driver.java
+++ src/main/java/io/github/mainstringargs/alpaca/hftish/Driver.java
@@ -1,9 +1,16 @@
 package io.github.mainstringargs.alpaca.hftish;
 
+import picocli.CommandLine;
+import picocli.CommandLine.ParseResult;
+
 public class Driver {
 
   public static void main(String[] args) {
 
+    CommandLine commandLine = new CommandLine(new AlgoConfig());
+    ParseResult parsed = commandLine.parseArgs(args);
+    AlgoConfig algoConfig = (AlgoConfig) parsed.commandSpec().userObject();
+    System.out.println(algoConfig);
 
   }
 
