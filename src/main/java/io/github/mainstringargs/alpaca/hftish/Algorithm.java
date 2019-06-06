12bcbee75bf831b75f3ba6543183ef4e0f708911
mainstringargs <mainstringargs@users.noreply.github.com>
2019-06-06 16:30:40 -0500

    Updating to keep better track of total shares

--- src/main/java/io/github/mainstringargs/alpaca/hftish/Algorithm.java
+++ src/main/java/io/github/mainstringargs/alpaca/hftish/Algorithm.java
@@ -1,5 +1,6 @@
 package io.github.mainstringargs.alpaca.hftish;
 
+import java.text.DecimalFormat;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
@@ -62,6 +63,10 @@ public class Algorithm {
   private static final ScheduledExecutorService scheduledService =
       ExecutorTracer.newScheduledThreadPool(1);
 
+  /** The curr format. */
+  DecimalFormat currFormat = new DecimalFormat("'$'0.00");
+
+
   /**
    * Gets the algo config.
    *
@@ -123,10 +128,36 @@ public class Algorithm {
   }
 
 
+  /**
+   * Schedule next open.
+   *
+   * @param marketClock the market clock
+   */
   private void scheduleNextOpen(Clock marketClock) {
 
     long delay = ChronoUnit.MILLIS.between(LocalTime.now(),
         Utilities.fromDateTimeString(marketClock.getNextOpen()));
+
+    if (delay < 0) {
+
+      try {
+        marketClock = alpacaApi.getClock();
+      } catch (AlpacaAPIException e1) {
+        e1.printStackTrace();
+      }
+
+      LOGGER.info("Clock has not yet rolled over after market close, sleeping for 15 minutes");
+
+      try {
+        Thread.sleep(TimeUnit.MILLISECONDS.convert(15, TimeUnit.MINUTES));
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+
+      scheduleNextOpen(marketClock);
+
+      return;
+    }
 
     LOGGER.info("Market is closed. Will Open in "
         + TimeUnit.MINUTES.convert(delay, TimeUnit.MILLISECONDS) + " minutes");
@@ -248,8 +279,12 @@ public class Algorithm {
    * Close stream listeners.
    */
   protected void closeStreamListeners() {
-    polygonApi.removePolygonStreamListener(polygonStreamListener);
-    alpacaApi.removeAlpacaStreamListener(alpacaStreamListener);
+    if (polygonStreamListener != null) {
+      polygonApi.removePolygonStreamListener(polygonStreamListener);
+    }
+    if (alpacaStreamListener != null) {
+      alpacaApi.removeAlpacaStreamListener(alpacaStreamListener);
+    }
   }
 
 
@@ -298,7 +333,9 @@ public class Algorithm {
 
         try {
 
-          LOGGER.info("Buy " + 100 + " of " + algoConfig.getSymbol() + " at $" + quote.getAsk());
+          LOGGER.info("Buy " + 100 + " of " + algoConfig.getSymbol() + " at "
+              + currFormat.format(quote.getAsk()) + "; Current shares: "
+              + position.getTotalShares());
 
           Order order = alpacaApi.requestNewOrder(algoConfig.getSymbol(), 100, OrderSide.BUY,
               OrderType.LIMIT, OrderTimeInForce.DAY, quote.getAsk(), null, null);
@@ -328,8 +365,9 @@ public class Algorithm {
 
         try {
 
-          LOGGER.info(
-              "Sell " + numberToSell + " of " + algoConfig.getSymbol() + " at $" + quote.getAsk());
+          LOGGER.info("Sell " + numberToSell + " of " + algoConfig.getSymbol() + " at "
+              + currFormat.format(quote.getAsk()) + "; Current shares: "
+              + position.getTotalShares());
 
           Order order = alpacaApi.requestNewOrder(algoConfig.getSymbol(), (int) numberToSell,
               OrderSide.SELL, OrderType.LIMIT, OrderTimeInForce.DAY, quote.getBid(), null, null);
@@ -360,9 +398,11 @@ public class Algorithm {
     if (message.getEvent() == OrderEvent.FILL) {
 
       if (message.getOrder().getSide().trim().equalsIgnoreCase("Buy")) {
-        position.updateTotalShares(Integer.parseInt(message.getOrder().getFilledQty()));
+        position.updateFilledAmount(message.getOrder().getId(),
+            Integer.parseInt(message.getOrder().getFilledQty()), message.getOrder().getSide());
       } else {
-        position.updateTotalShares(-1 * Integer.parseInt(message.getOrder().getFilledQty()));
+        position.updateFilledAmount(message.getOrder().getId(),
+            Integer.parseInt(message.getOrder().getFilledQty()), message.getOrder().getSide());
       }
 
       position.removePendingOrder(message.getOrder().getId(),
